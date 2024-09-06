57e56c1f83dd3cc98aff7ae4b9b9a530440c1a83
Ajay Joshi <96332170+aj9045j@users.noreply.github.com>
2024-09-06 13:26:40 +0530

    Update sptofiy.js

--- backend/keys/sptofiy.js
+++ backend/keys/sptofiy.js
@@ -1,9 +1,9 @@
 const SpotifyWebApi = require('spotify-web-api-node');
 
 const spotifyApi = new SpotifyWebApi({
-    clientId: '343ef971cb544d1f8dbbcaba0335bd25',
-    clientSecret: 'ce9d643c06e74aa580417759e2abe8ba',
-    redirectUri: 'http://15.207.196.70' // Update with your redirect URI
+    clientId: 'process.env.SPOTIFY_CLIENT_ID',
+    clientSecret: 'process.env.SPOTIFY_CLIENT_SECRET',
+    redirectUri: 'process.env.DOMAIN_NAME' // Update with your redirect URI
 });
 
 module.exports = {
